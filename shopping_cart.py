8229f56973ed74e6a5c54fd0fbe9972f0be3058e
Ron Lodetti Jr <ron.lodetti@gmail.com>
2023-07-12 09:04:48 -0400

    finished lab

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,17 +1,39 @@
+import numpy as np
 class ShoppingCart:
     # write your code here
-    def __init__(self, emp_discount=None):
-      pass
+    def __init__(self,employee_discount = None):
+        self.total = 0
+        self.employee_discount = employee_discount
+        self.items = []
+      
     def add_item(self, name, price, quantity=1):
-       pass
+        for i in list(range(quantity)):
+            self.items.append({"name": name, "price": price})
+            self.total += price
+        return self.total
+    
     def mean_item_price(self):
-       pass
+        prices = []
+        for i in self.items:
+            prices.append(i['price'])
+        return np.mean(prices)
 
     def median_item_price(self):
-        pass
+        prices = []
+        for i in self.items:
+            prices.append(i['price'])
+        return np.median(prices)
 
     def apply_discount(self):
-       pass
+        if self.employee_discount == None:
+            return "Sorry, there is no discount to apply to your cart :("
+        else:
+            disc_total = self.total * (1-self.employee_discount/100)
+            return disc_total
+            
 
     def void_last_item(self):
-        pass
+        if self.items == []:
+            return "There are no items in your cart!"
+        else:
+            self.total -= self.items.pop()['price']
